2e06641567d99cdf81c26f8e9cc11bd5a1ed85ae
Yossi <saoron@gmail.com>
2018-03-07 16:15:56 +0200

    better handle setSettings boolean params

--- server/api/setSetting/setSetting.controller.js
+++ server/api/setSetting/setSetting.controller.js
@@ -1,32 +1,33 @@
 'use strict';
 
-var path = require("path"),
-  fs = require('fs')
+var path = require('path'),
+	fs = require('fs');
 
+exports.index = function(req, res) {
+	var fieldName = req.param('fieldName');
+	var fieldValue = req.param('fieldValue');
 
-exports.index = function (req, res) {
+	var defaults = path.join(__dirname, '../../../..', 'config.json');
+	var config = JSON.parse(fs.readFileSync(defaults, 'utf-8'));
 
+	//cast to boolean if needed
+	if (fieldValue == 'true') fieldValue = true;
+	if (fieldValue == 'false') fieldValue = false;
 
-  var fieldName = req.param('fieldName');
-  var fieldValue = req.param('fieldValue');
+	fieldValue = fieldValue.toLowerCase();
 
-  var defaults = path.join(__dirname, '../../../..', 'config.json');
-  var config = JSON.parse(fs.readFileSync(defaults, 'utf-8'))
+	config['settings'][fieldName] = fieldValue;
 
-  config['settings'][fieldName] = fieldValue;
-
-  try {
-    fs.writeFileSync(defaults, JSON.stringify(config));
-    res.json({
-      "status": "1",
-      "reason": "setting was saved successfully!"
-    });
-  } catch (err) {
-    res.json({
-      "status": "0",
-      "reason": err
-    });
-  }
-
-
+	try {
+		fs.writeFileSync(defaults, JSON.stringify(config));
+		res.json({
+			status: '1',
+			reason: 'setting was saved successfully!'
+		});
+	} catch (err) {
+		res.json({
+			status: '0',
+			reason: err
+		});
+	}
 };
