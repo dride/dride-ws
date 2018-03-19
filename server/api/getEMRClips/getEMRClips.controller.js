3de738b41d2e0a2a325df1e07f69bc445a8fa50b
Yossi <saoron@gmail.com>
2018-03-19 10:47:54 +0200

    fix EMR clips format for Dride

--- server/api/getEMRClips/getEMRClips.controller.js
+++ server/api/getEMRClips/getEMRClips.controller.js
@@ -11,8 +11,20 @@ exports.index = function(req, res) {
 	var savedVideos = '/home/core/modules/video/savedVideos.json';
 	if (fs.existsSync(savedVideos)) {
 		var EMRvideos = fs.readFileSync(savedVideos, 'utf8');
+
+		try {
+			EMRvideos = JSON.parse(EMRvideos);
+		} catch (e) {
+			EMRvideos = [];
+		}
+		for (var i = 0; i < EMRvideos.length; i++) {
+			EMRvideos[i].timestamp = EMRvideos[i].key;
+			EMRvideos[i].clip = '/clip/' + EMRvideos[i].key + '.mp4';
+			EMRvideos[i].thumb = '/thumb/' + EMRvideos[i].key + '.jpg';
+		}
+
 		res.json({
-			data: JSON.parse(EMRvideos)
+			data: EMRvideos
 		});
 	} else {
 		res.json({
